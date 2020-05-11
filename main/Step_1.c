d95152056fe3ac01c64f862e0feaf1c18833f15c
MortencKruuse <s183681@student.dtu.dk>
2020-05-11 16:40:21 +0200

    Added not

--- main/Step_1.c
+++ main/Step_1.c
@@ -8,6 +8,7 @@ void calculateValueInBits(char *bits, int startBit, int bitsAvailable, int value
 char *machineCodeConverter(const char *string) {
     //Empty command
     char *bits = "0000000000000000\n\0";
+    int offset = 0;
     //Set all 'bits' to 0 but not \n and \0 value at [16] and [17]
     resetCharArray(bits, 16);
     //OP code first
@@ -25,13 +26,22 @@ char *machineCodeConverter(const char *string) {
             if (string[11] == '-')
                 bits[11] = '1';
         }
+        offset++;
 
     }
         //NOT
     else if (string[0] == 'N') {
-        bits[0] = '1';
-
-        bits[3] = '1';
+            bits[0] = '1';
+            bits[3] = '1';
+            calculateDirectoryInBits(bits, 4, (string[5] - '0'));
+            calculateDirectoryInBits(bits, 7, (string[8] - '0'));
+            bits[10] = '1';
+            bits[11] = '1';
+            bits[12] = '1';
+            bits[13] = '1';
+            bits[14] = '1';
+            bits[15] = '1';
+            offset++;
     }
         //BR
     else if (string[0] == 'B') {
@@ -43,6 +53,7 @@ char *machineCodeConverter(const char *string) {
         if (string[2] == 'p' || string[3] == 'p' || string[4] == 'p')
             bits[6] = '1';
         //PCoffset9 here
+        //Step 2. Need label for it
     }
         //LDR
     else if (string[0] == 'L') {
@@ -51,16 +62,22 @@ char *machineCodeConverter(const char *string) {
         if (string[2] == 'R') {
             bits[1] = '1';
         }
-            //LD (is still here to show LD is considered)
+        //PCoffset9 here
+        //BaseR is where we start from. 3000x
+        //Step 2. Need label for it
+
+
+        //LD (is still here to show LD is considered)
         else {
             bits[1] = '0';
         }
     }
         //ST
     else if (string[0] == 'S') {
-
         bits[2] = '1';
         bits[3] = '1';
+        //PCoffset9 here
+        //Step 2. Need label for it
     }
     return bits;
 }
